a06808c8e195fb5531473c147ed67faef4aa65ed
Vishal sreevaatsav <vishalvatsav@gmail.com>
2021-06-28 09:52:21 +0530

    Assignment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./page";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
